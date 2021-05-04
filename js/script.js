cc809a25d1267f22351c4e47ca8e985608d83fb9
Andry <and_rei0@mail.ru>
2021-05-04 21:06:54 +0300

    chnage

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ $(document).ready(function(){
 })
 
 
+/**Modal thanks state partners */
+const btn_modal_partners = document.querySelector('#btn_modal_partners');
+
+btn_modal_partners.addEventListener('click', () => {
+  btn_modal_partners.setAttribute('data-toggle', 'modal');
+  btn_modal_partners.setAttribute('data-dismiss', 'modal');
+})
+
+/**Modal thanks one */
+const btn_modal_one = document.querySelector('#btn_modal_one');
+
+btn_modal_one.addEventListener('click', () => {
+  btn_modal_one.setAttribute('data-toggle', 'modal');
+  btn_modal_one.setAttribute('data-dismiss', 'modal');
+})
+
+/**Modal thanks two */
+const btn_modal_two = document.querySelector('#btn_modal_two');
+
+btn_modal_two.addEventListener('click', () => {
+  btn_modal_two.setAttribute('data-toggle', 'modal');
+  btn_modal_two.setAttribute('data-dismiss', 'modal');
+})
+
