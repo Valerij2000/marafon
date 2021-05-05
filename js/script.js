33aafcfacfa0c140891444de4b2c24474abd4d2e
Andry <and_rei0@mail.ru>
2021-05-05 17:22:09 +0300

    change

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@ $(document).ready(function(){
   })
 })
 
+
+/**Modal state partners */
+
+
+
+  
+var state__partn__modal = new bootstrap.Modal(document.getElementById('state__partn__modal'), {
+  keyboard: false
+})
+
+var thanks = new bootstrap.Modal(document.getElementById('thanks'), {
+  keyboard: false
+})
+
+const state_partners_open_modal = document.querySelector('#state_partners_open_modal');
+state_partners_open_modal.addEventListener('click', function (event) {
+  state__partn__modal.show()
+})  
+
+const btn_modal_partners = document.querySelector('#btn_modal_partners');
+
+
+/**
+*   btn_modal_partners.addEventListener('click', () => {
+state__partn__modal.hide()
+thanks.show()
+})
+*/
