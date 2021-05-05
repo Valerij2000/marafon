acdba6f6f0d4836bbc7588ddf5c500224933f69b
Andry <and_rei0@mail.ru>
2021-05-05 12:16:19 +0300

    modal create

--- js/script.js
+++ js/script.js
@@ -13,16 +13,15 @@ $(document).ready(function(){
   })
 })
 
-
-/**Modal thanks state partners */
-const btn_modal_partners = document.querySelector('#btn_modal_partners');
+/**
+ * const btn_modal_partners = document.querySelector('#btn_modal_partners');
 
 btn_modal_partners.addEventListener('click', () => {
   btn_modal_partners.setAttribute('data-toggle', 'modal');
   btn_modal_partners.setAttribute('data-dismiss', 'modal');
 })
 
-/**Modal thanks one */
+
 const btn_modal_one = document.querySelector('#btn_modal_one');
 
 btn_modal_one.addEventListener('click', () => {
@@ -30,11 +29,47 @@ btn_modal_one.addEventListener('click', () => {
   btn_modal_one.setAttribute('data-dismiss', 'modal');
 })
 
-/**Modal thanks two */
+
 const btn_modal_two = document.querySelector('#btn_modal_two');
 
 btn_modal_two.addEventListener('click', () => {
   btn_modal_two.setAttribute('data-toggle', 'modal');
   btn_modal_two.setAttribute('data-dismiss', 'modal');
 })
+
+ * document.querySelector('#btn_modal_partners').setAttribute('data-toggle', 'modal');
+ * 
+ */
+
+/**
+ * const btn_modal_partners = document.querySelector('#btn_modal_partners');
+btn_modal_partners.addEventListener('click', () => {
+  document.querySelector('#state__partn__modal').style.display = 'none';
+})
+
+ */
+
+/**Modal state partners */
+
+var state__partn__modal = new bootstrap.Modal(document.getElementById('state__partn__modal'), {
+  keyboard: false
+})
+
+var thanks = new bootstrap.Modal(document.getElementById('thanks'), {
+  keyboard: false
+})
+
+const state_partners_open_modal = document.querySelector('#state_partners_open_modal');
+state_partners_open_modal.addEventListener('click', function (event) {
+  state__partn__modal.show()
+})  
+
+const btn_modal_partners = document.querySelector('#btn_modal_partners');
+btn_modal_partners.addEventListener('click', () => {
+  state__partn__modal.hide()
+  thanks.show()
+})
+
+
+
 
