ad13c780ba65b9aa8e52fc4b03052f8626c21966
Andry <and_rei0@mail.ru>
2021-05-06 00:42:54 +0300

    changed modal thanks

--- js/script.js
+++ js/script.js
@@ -30,9 +30,7 @@ state_partners_open_modal.addEventListener('click', function (event) {
 
 const btn_modal_partners = document.querySelector('#btn_modal_partners');
 
-/**
-*   btn_modal_partners.addEventListener('click', () => {
+btn_modal_partners.addEventListener('click', () => {
 state__partn__modal.hide()
 thanks.show()
 })
-*/
