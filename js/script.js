67f1e2607ea94dc5c6ab086165da524038382709
Andry <and_rei0@mail.ru>
2021-05-14 09:26:45 +0300

    new change scrips

--- js/script.js
+++ js/script.js
@@ -8,36 +8,86 @@ function scrollDownUp(id){
 };
 // Burger Menu
 $(document).ready(function(){
+
+  console.log(document.documentElement.clientWidth);
+
+  // yt player plugin
+
+  var player;
+  let video_width = '700px';
+  if (document.documentElement.clientWidth < 700){
+    video_width = '350px'
+  }
+
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video_player_box', {
+      height: video_width,
+      width: '100%',
+      videoId: 'LkxvQTSIzUs',
+      playerVars: { 'autoplay': 0 },
+      events: {
+        // 'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+/*
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+*/
+
+  var done = false;
+  function onPlayerStateChange(event) {
+
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
+
+  setTimeout(onYouTubeIframeAPIReady, 3000);
+  console.log(player)
+
+  $('#close_modal_video_btn').on('click', function (e){
+    stopVideo();
+  });
+
+  // end yt player plugin
+
   $('.header__burger').click(function () {
     $('.header__burger, .header__menu').toggleClass('active');
+  });
+
+  /**Modal state partners */
+  var state__partn__modal = new bootstrap.Modal(document.getElementById('state__partn__modal'), {
+    keyboard: false
+  })
+
+  var thanks = new bootstrap.Modal(document.getElementById('thanks'), {
+    keyboard: false
+  })
+
+  const state_partners_open_modal = document.querySelector('#state_partners_open_modal');
+  if(state_partners_open_modal){
+    state_partners_open_modal.addEventListener('click', function (event) {
+      state__partn__modal.show()
+    })
+  }
+
+
+  const state_partners_open_modal_footer = document.querySelector('#state_partners_open_modal_footer');
+  state_partners_open_modal_footer.addEventListener('click', function (event) {
+    state__partn__modal.show()
+  })
+
+  const btn_modal_partners = document.querySelector('#btn_modal_partners');
+
+
+  btn_modal_partners.addEventListener('click', () => {
+    state__partn__modal.hide()
+    thanks.show()
   })
 })
 
-/**Modal state partners */
-  
-var state__partn__modal = new bootstrap.Modal(document.getElementById('state__partn__modal'), {
-  keyboard: false
-})
 
-var thanks = new bootstrap.Modal(document.getElementById('thanks'), {
-  keyboard: false
-})
-
-const state_partners_open_modal = document.querySelector('#state_partners_open_modal');
-state_partners_open_modal.addEventListener('click', function (event) {
-  state__partn__modal.show()
-})  
-
-const state_partners_open_modal_footer = document.querySelector('#state_partners_open_modal_footer');
-state_partners_open_modal_footer.addEventListener('click', function (event) {
-  state__partn__modal.show()
-}) 
-
-const btn_modal_partners = document.querySelector('#btn_modal_partners');
-
-
-btn_modal_partners.addEventListener('click', () => {
-state__partn__modal.hide()
-thanks.show()
-})
  
